2cee80980ecc197407a1ddaebbdb80c22995b25e
picronicro <eliyaz2761@gmail.com>
2023-08-29 23:23:35 +0600

    hw 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function RegForm() {
+  const [name, setName] = useState("")
+  const [surname, setSurname] = useState("")
+  const [patronymic, setPatronymic] = useState("")
+  const [email, setEmail] = useState("")
+
+  return(
+      <div>
+        <div className="form">
+          <label htmlFor="name">name</label>
+          <input id="name" type="text" onChange={e => setName(e.target.value)}/>
+
+          <label htmlFor="surname">surname</label>
+          <input id="surname" type="text" onChange={e => setSurname(e.target.value)}/>
+
+          <label htmlFor="patronymic">patronymic</label>
+          <input id="patronymic" type="text" onChange={e => setPatronymic(e.target.value)}/>
+
+          <label htmlFor="email">email</label>
+          <input id="email" type="text" onChange={e => setEmail(e.target.value)}/>
+        </div>
+        <div className="user_card">
+            <p className="name">name: {name}</p>
+            <p className="surname">name: {surname}</p>
+            <p className="name">patronymic: {patronymic}</p>
+            <p className="name">email: {email}</p>
+        </div>
+      </div>
+  )
 }
-
-export default App;
